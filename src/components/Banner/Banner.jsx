b343c2171dc0c14376d42061d5af1ea866c7d571
nuri-cloud <nurzadatobokelova@gmail.com>
2025-04-01 12:33:26 +0600

    echteke

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div>
+      kushtar
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, corporis fuga culpa odit ullam iusto necessitatibus aliquid unde qui fugiat, totam cumque deserunt, repellendus earum quas sed cupiditate dolorem magni.
+        gsldfhwoahifuhew
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+        hjfkwe
+      </p>
+    </div>
+  )
+}
+
+export default Banner
